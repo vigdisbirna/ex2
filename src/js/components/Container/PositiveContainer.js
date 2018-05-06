2f8f743d79b4d63462871c31e12bf322e944ff49
Hanna <hanna15@ru.is>
2018-05-06 13:08:24 +0000

    positive

--- src/js/components/Container/PositiveContainer.js
+++ src/js/components/Container/PositiveContainer.js
@@ -4,14 +4,29 @@ import PropTypes from 'prop-types';
 import Image from "../Image/Image";
 
 export default class PositiveContainer extends React.Component {
-
-
+	constructor() {
+		super();
+		this.state = {pos: []};
+	}
+	componentDidUpdate(prevProps, prevState) {
+		if(this.state.pos != prevState.pos) {
+			var newElem = this.state.pos[this.state.pos.length - 1];
+			console.log('in child, new elem is');
+			console.log(newElem);
+			this.props.callBackFromParent(newElem);
+		}
+	}
+	changeOnClick() {
+		this.setState(prevState => ({
+			pos: [...prevState.pos, 1]
+		}));
+	}
     render() {
         return (
             <div>
                 <h5 className="d-flex justify-content-center">Positive</h5>
+                <p onClick={this.changeOnClick.bind(this)}>This is pos list</p>
             </div>
         );
-
     }
 }
