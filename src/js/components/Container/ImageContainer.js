4ad33c7d38b137c56b1606ef2b941bbda3f0afb1
Thorhildur <thorhildurt15@ru.is>
2018-05-09 16:47:56 +0000

    fix

--- src/js/components/Container/ImageContainer.js
+++ src/js/components/Container/ImageContainer.js
@@ -13,14 +13,14 @@ export default class ImageContainer extends React.Component {
 
     constructor(props) {
         super(props);
-       this.state = {image_arr: [], vis_arr: [],  pos_arr: [], neg_arr: [], interval: null, pos_cnt: 0, neg_cnt: 0};
-        //this.state = {image_arr: Array.from(Array(50).keys()), vis_arr: Array.from(Array(50).keys()),  pos_arr: [], neg_arr: [], interval: null, pos_cnt: 0, neg_cnt: 0};
+       //this.state = {image_arr: [], vis_arr: [],  pos_arr: [], neg_arr: [], interval: null, pos_cnt: 0, neg_cnt: 0};
+        this.state = {image_arr: Array.from(Array(50).keys()), vis_arr: Array.from(Array(50).keys()),  pos_arr: [], neg_arr: [], interval: null, pos_cnt: 0, neg_cnt: 0};
     }
 
     componentDidMount() {
-        this.initialize();        
+       /* this.initialize();        
         var intv = setInterval(this.loadData.bind(this), 40000);
-        this.setState({interval: intv});
+        this.setState({interval: intv});*/
     }
 
     componentWillUnmount() {
